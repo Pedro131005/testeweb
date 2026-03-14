5ab52e55425644398929354bd84420aeab06cd7c
Pedro Bernardo Cassoto <pedrocassoto05@gmail.com>
2026-03-13 21:45:48 -0300

    aula_13032026

---
+++ aula13032026/index.php
@@ -0,0 +1,17 @@
+<?php
+   echo"Olá mundo!!!";
+   // Variáveis 
+   $nome = "Pedro";
+   $rgm = 34353545;
+   echo '<p>Olá '.$nome.' seu RGM é ' .$rgm.'</p>';
+   echo "Olá $nome! Seu RGM é $rgm <br>"; 
+   // Ver o tipo da variável
+   echo gettype($rgm);
+   $a = 10;
+   $b = 5;
+   echo $a + $b;
+  // echo $a + $nome; //
+?>
+<a href="formulario.html" target="_blank">
+    Clique aqui para ver o formulário
+</a>
